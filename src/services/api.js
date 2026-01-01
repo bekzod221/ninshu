436c4fba2d98ee2e5556a69fe6f4f55542d7833f
Bekzod <162580380+bekzod221@users.noreply.github.com>
2026-01-01 18:31:01 +0500

    player change

--- src/services/api.js
+++ src/services/api.js
@@ -122,14 +122,14 @@ export const transformAnimeData = (apiAnime, episodeCount = 0) => {
  * CVH is typically ad-free and newer, so highest priority
  */
 const PLAYER_PRIORITY = {
-  'Плеер CVH': 4,
-  'CVH': 4,
-  'Плеер Alloha': 3,
-  'Alloha': 3,
-  'Плеер Kodik': 2,
-  'Kodik': 2,
-  'Плеер Aksor': 1,
-  'Aksor': 1,
+  'Плеер CVH': 0,
+  'CVH': 0,
+  'Плеер Alloha': 10,
+  'Alloha': 10,
+  'Плеер Kodik': 0,
+  'Kodik': 0,
+  'Плеер Aksor': 0,
+  'Aksor': 0,
 };
 
 /**
